d6add268d4512e970d87f8cfdf6dad744aabe724
“Ricky <grickyh@gmail.com>
2021-01-18 04:05:42 +0700

    Updated for loopto create one div class for grid and added a mouseover function to draw on grid

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+// Bindings for grid values
 const container = document.getElementById("container");
-// variables for rowcell and coll cell
 const rowCell = document.getElementsByClassName("rowCell");
 const colCell = document.getElementsByClassName("colCell")
 
@@ -8,29 +8,31 @@ const colCell = document.getElementsByClassName("colCell")
 
 const createGrid = num => {
 // For loop through num
-    for(let i = 0; i < num; i++) {
-// Create div for grid row
-        let row = document.createElement('div');
-// Append row div too container with class name rowCell
-        container.appendChild(row).className = "rowCell";
-    }
-// For loop through length of rowCell
-    for(let j = 0; j < rowCell.length; j++) {
-// For loop through num
-        for(let c = 0; c < num; c++) {
+        for(let i = 0; i < num; i++) {
+// Nested for loop through num
+            for(let c = 0; c < num; c++) {
 // Create div for cell
-            let cell = document.createElement('div');
-// Append cell too rowCell index 
-            rowCell[j].appendChild(cell).className = "colCell";
-
+                let cell = document.createElement('div');
+// Append cell too container
+                container.appendChild(cell).className = "cell";
+            }
         }
+    };
     
-    }
-
-};
-
 // Call function
+    createGrid(16);
 
-createGrid(16);
+// Console log childNodes
 
 console.log(container.childNodes);
+
+// Function too change grid color on mouse hover
+
+container.addEventListener("mouseover", draw)
+
+function draw(e) {  
+    console.log(e);
+    
+        e.target.style.backgroundColor = "green";
+    }
+
